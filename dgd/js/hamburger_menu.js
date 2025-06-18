78b35f80b8e1012c7a60207af3ab69f6226a0098
Marcos Matavel <matavelmarcos045@gmail.com>
2025-06-18 19:23:49 +0200

    changing the sign page layout

--- dgd/js/hamburger_menu.js
+++
@@ -1,9 +0,0 @@
-// Select elements
-const hamburgerMenu = document.getElementById('hamburger_menu');
-const navLinks = document.querySelector('.nav-links');
-
-// Toggle menu on click
-hamburgerMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    hamburgerMenu.classList.toggle('open');
-});
